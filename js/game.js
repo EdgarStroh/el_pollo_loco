11604fb560ba71abc7d983809de2c7d3b094fd63
EdgarStroh <edgarstroh@googlemail.com>
2024-10-14 10:12:01 +0200

    added keyboard movement

--- js/game.js
+++ js/game.js
@@ -1,11 +1,48 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
-function init(){
+function init() {
     canvas = document.getElementById("canvas");
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
     ctx = canvas.getContext('2d');
 
     console.log('My character is ', world.character);
-    
+
 }
+
+window.addEventListener("keydown", (event) => {
+    if (event.keyCode == 39 || event.key == 'd') {
+        keyboard.RIGHT = true;
+    }
+    if (event.keyCode == 37 || event.key == 'a') {
+        keyboard.LEFT = true;
+    }
+    if (event.keyCode == 40 || event.key == 's') {
+        keyboard.DOWN = true;
+    }
+    if (event.keyCode == 38 || event.key == 'w') {
+        keyboard.UP = true;
+    }
+    if (event.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+});
+
+window.addEventListener("keyup", (event) => {
+    if (event.keyCode == 39 || event.key == 'd') {
+        keyboard.RIGHT = false;
+    }
+    if (event.keyCode == 37 || event.key == 'a') {
+        keyboard.LEFT = false;
+    }
+    if (event.keyCode == 40 || event.key == 's') {
+        keyboard.DOWN = false;
+    }
+    if (event.keyCode == 38 || event.key == 'w') {
+        keyboard.UP = false;
+    }
+    if (event.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+});
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -13,19 +13,26 @@ class World {
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-        
+
         // new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 420)
-        
+
     ];
     canvas;
     ctx;
+    keyboard;
 
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
+        this.setWorld();
     }
 
+    setWorld(){
+        this.character.world = this;
+    }
+    
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
